467aad61f0b11b398a13bb7caab6c69ff1e51d85
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2024-02-29 21:26:29 +0530

    Update Even Odd Tree.cpp

--- Tree/Even Odd Tree.cpp
+++ Tree/Even Odd Tree.cpp
@@ -1,1 +1,40 @@
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    bool isEvenOddTree(TreeNode* root) {
+        queue<TreeNode*>q;
+        q.push(root);
+        int level=0;
 
+        while(!q.empty()){
+            int N=q.size();
+            int prev_val;
+            if(level%2==0){
+                prev_val=INT_MIN;
+            }
+            else{
+                prev_val=INT_MAX;
+            }
+
+            while(N--){
+                TreeNode* u=q.front();
+                q.pop();
+
+                if(level%2==0){
+                    if(u->val%2==0 || u->val<=prev_val) return false;
+                }
+                else{
+                    if(u->val%2!=0 || u->val>=prev_val) return false;
+                }
+
+                prev_val=u->val;
+
+                if(u->left) q.push(u->left);
+                if(u->right) q.push(u->right);
+            }
+            level++;
+        }
+        return true;
+    }
+};
