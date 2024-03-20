5d1e8c2bb218b02f3a06f4517ebf066f93375229
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2024-03-20 11:15:27 +0530

    Create Merge In Between Linked Lists.cpp

---
+++ Linkedlist/Merge In Between Linked Lists.cpp
@@ -0,0 +1,27 @@
+//T.C : O(m+n)
+//S.C : O(1)
+class Solution {
+public:
+    ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
+        ListNode* left  = NULL;
+        ListNode* right = list1;
+        
+        for(int i = 0; i <= b; i++) {
+            if(i == a-1) {
+                left = right;
+            }
+            right = right->next;
+        }
+        
+        left->next = list2;
+        
+        ListNode* temp = list2;
+        while(temp && temp->next) {
+            temp = temp->next;
+        }
+        
+        temp->next = right;
+        
+        return list1;  
+    }
+};
