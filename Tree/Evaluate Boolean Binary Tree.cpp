94e7a18c94ee7124920fd6b7598fb4b466aeb091
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2024-05-16 17:56:48 +0530

    Create Evaluate Boolean Binary Tree.cpp

---
+++ Tree/Evaluate Boolean Binary Tree.cpp
@@ -0,0 +1,16 @@
+//T.C : O(n)
+//S.C : O(depth of tree)
+class Solution {
+public:
+    bool evaluateTree(TreeNode* root) {
+        if(root->left == NULL && root->right == NULL) {
+            return root->val;
+        }
+        
+        if(root->val == 2) {
+            return evaluateTree(root->left) | evaluateTree(root->right);
+        }
+        
+        return evaluateTree(root->left) & evaluateTree(root->right);
+    }
+};
