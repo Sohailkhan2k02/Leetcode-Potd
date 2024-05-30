e7f4de3229a7c485b3cf365a1f922b7fdb232d3a
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2024-05-30 10:44:03 +0530

    Create Count Triplets That Can Form Two Arrays of Equal XOR.cpp

---
+++ Bit Manipulation/Count Triplets That Can Form Two Arrays of Equal XOR.cpp
@@ -0,0 +1,30 @@
+//Approach (Better than brute force)
+//T.C : O(n^2)
+//S.C : O(n)
+class Solution {
+public:
+    int countTriplets(vector<int>& arr) {
+        vector<int> prefixXor(begin(arr), end(arr));
+
+        prefixXor.insert(prefixXor.begin(), 0); //initially the xor cumulative will be 0
+        int n = prefixXor.size();
+
+        for(int i = 1; i < n; i++) {
+            prefixXor[i] ^= prefixXor[i-1];
+        }
+
+        int triplets = 0;
+
+        for(int i = 0; i < n; i++) {
+            for(int k = i+1; k < n; k++) {
+
+                if(prefixXor[k] == prefixXor[i]) {
+                    triplets += k-i-1;
+                }
+
+            }
+        }
+
+        return triplets;
+    }
+};
