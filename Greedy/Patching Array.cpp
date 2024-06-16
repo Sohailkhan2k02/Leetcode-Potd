c7d38e6a37ff23008e544fd6fda3f9b22c794dc0
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2024-06-16 17:43:11 +0530

    Create Patching Array.cpp

---
+++ Greedy/Patching Array.cpp
@@ -0,0 +1,20 @@
+//T.C : O(max(l, log(n)), l = length of nums
+//S.C : O(1)
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        long maxReach = 0,cnt = 0;
+        int i = 0;
+
+        while(maxReach < n) {
+            if(i < nums.size() && nums[i] <= maxReach+1) {
+               maxReach += nums[i];
+               i++;
+            } else {
+               maxReach += (maxReach + 1);
+               cnt++;
+            }
+        }
+        return cnt;
+    }
+};
