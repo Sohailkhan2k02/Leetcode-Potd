cfdb225d75ab22622ca907e9968906e7132c54fc
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2024-09-21 09:31:17 +0530

    Create Lexicographical Numbers.cpp

---
+++ Number Theory/Lexicographical Numbers.cpp
@@ -0,0 +1,36 @@
+//T.C : O(n*logn)
+//S.C : O(n)
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int>res;
+        for(int i=1; i<=n; i++){
+            res.push_back(i);
+        }
+        sort(res.begin(),res.end(),[&](int a, int b){
+            return to_string(a)<to_string(b);
+        });
+        return res;
+    }
+};
+
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int>res;
+        int curr =1;
+        while (res.size() < n){
+            res.push_back(curr);
+            if (curr * 10<=n){
+                curr *= 10;
+            }else{
+                while (curr % 10==9 || curr == n)
+                curr /= 10;
+                ++curr;
+            }
+        }
+        return res;
+    }
+};
