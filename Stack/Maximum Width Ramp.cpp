dd6b23fa055121f21052b45f4bffd6cfaa620fc5
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2024-10-10 08:55:17 +0530

    Create Maximum Width Ramp.cpp

---
+++ Stack/Maximum Width Ramp.cpp
@@ -0,0 +1,23 @@
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    int maxWidthRamp(vector<int>& nums) {
+        //decreasing stack
+        int n = nums.size();
+        stack<int> st;
+        for(int i = 0; i < n; i++){
+            if(st.empty() || nums[st.top()] > nums[i]){
+                st.push(i);
+            }
+        }
+        int ans = 0;
+        for(int i = n-1; i > 0; i--){
+            while(!st.empty() && nums[st.top()] <= nums[i]){
+                ans = max(ans, i - st.top());
+                st.pop();
+            }
+        }
+        return ans;
+    }
+};
