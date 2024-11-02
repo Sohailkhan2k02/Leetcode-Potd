e04534e8681f227c703a04869b3ed700ce3f3287
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2024-11-02 10:45:23 +0530

    Update Circular Sentence.cpp

--- Strings/Circular Sentence.cpp
+++ Strings/Circular Sentence.cpp
@@ -1,3 +1,4 @@
+//APPROACH -1 
 //T.C : O(n)
 //S.C : O(n)
 class Solution {
@@ -25,6 +26,24 @@ public:
             }
         }
         if(res[res.size()-1][1]!=res[0][0]) return false;
+        return true;
+    }
+};
+
+//APPROACH -2
+//T.C : O(N)
+//S.C : O(1)
+class Solution {
+public:
+    bool isCircularSentence(string s) {
+        int n=s.length();
+        if(s[0]!=s[n-1]) return false;
+        for(int i=1;i<n-1;i++){
+            char ch=s[i];
+            if(ch==' '){
+               if(s[i-1]!=s[i+1]) return false;
+            }
+        }
         return true;
     }
 };
