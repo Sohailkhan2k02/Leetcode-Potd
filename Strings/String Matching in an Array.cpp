53f4c13640c904ad6e581904433bfc9a5aa62968
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2025-01-07 08:27:22 +0530

    Create String Matching in an Array.cpp

---
+++ Strings/String Matching in an Array.cpp
@@ -0,0 +1,18 @@
+//T.C : O(n*n)
+//S.C : O(n)
+class Solution {
+public:
+    vector<string> stringMatching(vector<string>& words) {
+        vector<string> result;
+        int n = words.size();
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i != j && words[j].find(words[i]) != string::npos) {
+                    result.push_back(words[i]);
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+};
