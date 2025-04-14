b400b6fe97d10fc36c93fffff46c1a1205337a3b
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2025-04-14 11:46:12 +0530

    Create Count Good Triplets.cpp

---
+++ Math/Count Good Triplets.cpp
@@ -0,0 +1,18 @@
+//T.C : O(n*n*n)
+//S.C : O(1)
+class Solution {
+    public:
+        int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+            int good = 0;
+            for (int i = 0 ; i < arr.size() - 2 ; i++) {
+                for (int j = i + 1 ; j < arr.size() - 1 ; j++) {
+                    for (int k = j + 1; k < arr.size() ; k++) 
+                    if (abs(arr[i] - arr[j]) <= a && 
+                        abs(arr[j] - arr[k]) <= b
+                        && abs(arr[i] - arr[k]) <= c) 
+                        ++good; 
+                }
+            }
+            return good;
+        }
+    };
