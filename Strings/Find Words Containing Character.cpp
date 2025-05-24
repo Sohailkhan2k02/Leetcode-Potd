57edb3e82ead9dee4ca4ad676bc45b7de51a5347
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2025-05-24 17:20:18 +0530

    Create Find Words Containing Character.cpp

---
+++ Strings/Find Words Containing Character.cpp
@@ -0,0 +1,16 @@
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+         vector<int> indices;
+    
+    for (int i = 0; i < words.size(); ++i) {
+        if (words[i].find(x) != std::string::npos) {
+            indices.push_back(i);
+        }
+    }
+    
+    return indices;
+    }
+};
