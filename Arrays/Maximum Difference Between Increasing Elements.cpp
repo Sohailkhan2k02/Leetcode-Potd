cf482561a4e2f70ab864ea5d9605d992230d22fb
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2025-06-16 20:23:17 +0530

    Create Maximum Difference Between Increasing Elements.cpp

---
+++ Arrays/Maximum Difference Between Increasing Elements.cpp
@@ -0,0 +1,16 @@
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    int maximumDifference(vector<int>& nums) {
+        int xMin=nums[0], n=nums.size();
+        int maxD=-1;
+        for(int i=1; i<n; i++){
+            int x=nums[i];
+            if (x<=xMin)
+                xMin=x;
+            else maxD=max(maxD, x-xMin); 
+        }
+        return maxD;
+    }
+};
