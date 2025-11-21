c599ec48386f16e3a9641e2e0f9bfd0381d972b4
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2025-11-21 06:25:54 +0530

    Create Unique Length-3 Palindromic Subsequences.cpp

---
+++ Hash Table/Unique Length-3 Palindromic Subsequences.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+        int n=s.size();
+        vector<pair<int,int>>vec(26,{-1,-1});
+
+        for(int i=0; i<n; i++){
+            char ch=s[i];
+            int ind=ch-'a';
+
+            if(vec[ind].first==-1){
+                vec[ind].first=i;
+            }
+            vec[ind].second=i;
+        }
+
+        int res=0;
+        for(int i=0; i<26; i++){
+           int left=vec[i].first;
+           int right=vec[i].second;
+
+           if(left==-1) continue;
+
+           unordered_set<char>st;
+
+           for(int i=left+1; i<=right-1; i++){
+               st.insert(s[i]);
+           }
+           res+=st.size();
+        }
+
+        return res;
+    }
+};
