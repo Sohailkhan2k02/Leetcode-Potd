32bf45c60a85992f736a1ba3abee800611475845
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2026-01-05 17:05:01 +0530

    Create Maximum Matrix Sum.cpp

---
+++ Greedy/Maximum Matrix Sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        using ll=long long;
+        int n=matrix.size();
+        ll sum=0,mini=INT_MAX,neg=0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                mini=min(mini,(ll)abs(matrix[i][j]));
+                sum+=abs(matrix[i][j]);
+                if(matrix[i][j]<0) neg++;
+            }
+        }
+        if(neg&1){
+            sum-=2*mini;
+        }
+        return sum;
+    }
+};
