067c48024480a79091b25ca4310d8f21e14df5a3
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2026-04-05 15:27:55 +0530

    Create Robot Return to Origin.cpp

---
+++ Strings/Robot Return to Origin.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0, y = 0;
+        for(auto &i : moves) {
+            if(i == 'L') x--;
+            if(i == 'R') x++;
+            if(i == 'U') y++;
+            if(i == 'D') y--;
+        }
+        return x == 0 && y == 0;
+    }
+};
