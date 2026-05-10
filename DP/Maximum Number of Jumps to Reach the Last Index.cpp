5249369ac6277e1467d807849553df70dceaf922
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2026-05-10 17:27:43 +0530

    Create Maximum Number of Jumps to Reach the Last Index.cpp

---
+++ DP/Maximum Number of Jumps to Reach the Last Index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumJumps(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<int> dp(n,-1);
+        dp[0]=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(abs(nums[i]-nums[j])<=target && dp[j]!=-1){
+                    dp[i]=max(dp[j]+1,dp[i]);
+                }
+            }
+        }
+        return dp[n-1];
+    }
+};
