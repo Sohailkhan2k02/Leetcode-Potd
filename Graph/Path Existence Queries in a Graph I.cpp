4c5cd7a96b689784c610ea4ea946432a0a5c0ae6
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2026-07-09 18:20:44 +0530

    Create Path Existence Queries in a Graph I.cpp

---
+++ Graph/Path Existence Queries in a Graph I.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> parent,rnk;
+    int find(int i){
+        if(i==parent[i]) return i;
+        return parent[i]=find(parent[i]);// Path compression
+    }
+    void Union(int x,int y){
+        int parx=find(x);
+        int pary=find(y);
+        // Union by rank
+        if(rnk[parx]>rnk[pary]) parent[pary]=parx;
+        else if(rnk[parx]<rnk[pary]) parent[parx]=pary;
+        else{
+            parent[pary]=parx;
+            rnk[parx]++;
+        } 
+    }
+    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+        // Step 1: Initialize Union-Find
+        parent.resize(n);
+        rnk.resize(n,0);
+        for(int i=0;i<n;i++) parent[i]=i;
+
+        // Step 2: Connect components based on adjacent differences
+        for(int i=1;i<n;i++){
+            if(abs(nums[i]-nums[i-1])<=maxDiff) Union(i,i-1); // i and i-1 are in the same component
+        }
+
+        // Step 3: Answer queries
+        int m=queries.size();
+        vector<bool> ans(m);
+        for(int i=0;i<m;i++){
+            int x=queries[i][0],y=queries[i][1];
+            if(find(x)==find(y)) ans[i]=true;
+            else ans[i]=false;
+        }
+        return ans;
+    }
+};
