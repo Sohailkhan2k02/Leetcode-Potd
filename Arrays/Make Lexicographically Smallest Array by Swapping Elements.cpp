9c1baf1c4d987493a0d96385dc4544ed9fac672f
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2026-08-29 20:03:14 +0530

    Create Make Lexicographically Smallest Array by Swapping Elements.cpp

---
+++ Arrays/Make Lexicographically Smallest Array by Swapping Elements.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
+        vector<pair<int, int>> idxVals;
+        for (int i = 0; i < nums.size(); ++i) 
+            idxVals.push_back({nums[i], i});
+        
+        sort(idxVals.begin(), idxVals.end());
+
+        vector<vector<int>> groups = {{idxVals[0].second}};
+        for (int i = 1; i < nums.size(); ++i) {
+            if (idxVals[i].first - idxVals[i - 1].first <= limit)
+                groups.back().push_back(idxVals[i].second);
+            else
+                groups.push_back({idxVals[i].second});
+        }
+
+        for (auto& grp : groups) {
+            vector<int> vals;
+            for (int idx : grp) vals.push_back(nums[idx]);
+            sort(vals.begin(), vals.end());
+            sort(grp.begin(), grp.end());
+            for (int j = 0; j < grp.size(); ++j) nums[grp[j]] = vals[j];
+        }
+        return nums;
+    }
+};
