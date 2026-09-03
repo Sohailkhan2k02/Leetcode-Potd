994e4a441bd39c15b042a174f736247d4f9e98df
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2026-09-03 21:39:52 +0530

    Create Construct Uniform Parity Array II.cpp

---
+++ Arrays/Construct Uniform Parity Array II.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int minOdd = INT_MAX;
+        int minEven = INT_MAX;
+
+        for (int x : nums1) {
+            if (x % 2 == 0) {
+                minEven = min(minEven, x);
+            } else {
+                minOdd = min(minOdd, x);
+            }
+        }
+
+        if (minOdd == INT_MAX) {
+            return true;
+        }
+
+        return minOdd < minEven;
+    }
+};
