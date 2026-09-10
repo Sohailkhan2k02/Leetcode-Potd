ecdb3d427661bafe9e88761bca5118f8ff5039eb
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2026-09-10 20:55:39 +0530

    Create Count Nodes Equal to Average of Subtree.cpp

---
+++ Tree/Count Nodes Equal to Average of Subtree.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int result;
+    pair<int, int> solve(TreeNode* root) {
+        if(!root)
+            return {0, 0};
+        
+        pair<int, int> l = solve(root->left);
+        pair<int, int> r = solve(root->right);
+        
+        int leftSum   = l.first;
+        int leftCount = l.second;
+        
+        int rightSum   = r.first;
+        int rightCount = r.second;
+        
+        int SUM   = leftSum + rightSum + root->val;
+        int COUNT = leftCount + rightCount + 1;
+        
+        int avg = SUM/COUNT;
+        
+        if(avg == root->val) {
+            result++;
+        }
+        
+        return {SUM, COUNT};
+    }
+    
+    int averageOfSubtree(TreeNode* root) {
+        result = 0;
+        
+        solve(root);
+        
+        return result;
+    }
+};
